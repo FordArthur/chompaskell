187f35b9bf45756f33147e6ed7468e4cc9cfd937
Ford Arthur <jajasaludos.112358@gmail.com>
2024-09-07 20:03:05 +0200

    Added classes and such

--- src/Parser.hs
+++ src/Parser.hs
@@ -6,29 +6,42 @@ import Control.Arrow
 import Text.Parsec
 import Text.Parsec.Pos
 import qualified Data.HashMap.Strict as H
+import qualified Data.HashSet as S
 
-data TopLevel a =
-    Implementation { funcName         :: Node a, arguments    :: [Node a], body            :: [Node a]                    }
-  | Declaration    { declName         :: Node a, constraintsD    :: H.HashMap String [Node a], declType     :: Node a     }
-  | Data           { typeName         :: Node a, typeArgs     :: [Node a], constructors    :: [Node a]                    }
-  | Instance       { instancing       :: Node a, instanced    :: Node a, implementations :: H.HashMap String [TopLevel a] }
-  | Class          { className        :: Node a, declarations :: H.HashMap String [TopLevel a]                            }
+data TopLevel =
+    Implementation { funcName         :: Node, arguments    :: [Node], body            :: [Node]                    }
+  | Declaration    { declName         :: Node, constraintsD    :: H.HashMap String [Node], declType     :: Node     }
+  | Data           { typeName         :: Node, constructors    :: [Node]                    }
+  | Instance       { instancing       :: Node, instanced    :: Node, implementations :: H.HashMap String [TopLevel] }
+  | Class          { className        :: Node, declarations :: H.HashMap String TopLevel                            }
   | Infix
 
 data AtomType = Identifier | TypeK | Operator | Nat | Real' | Char' | String' | EndOfFile
   deriving (Eq, Show)
 
-data Node a =
-    Atom           { name       :: String, atomType    :: AtomType , info :: a , pos :: SourcePos        }
-  | Lambda         { args       :: [Node a], lamExpr     :: Node a                                         }
-  | Expression     { expression :: [Node a]                                                              }
-  | BinaryExpr     { leftExpr   :: Node a, oper        :: Node a   , rightExpr   :: Node a               }
-  | ConcType       { concName   :: Node a, consArgs    :: [Node a]                                       }
-  | TypeAssertion  { node       :: Node a, constraints :: H.HashMap String [Node a], choritype :: Node a }
-  | LocalBind      { locName    :: Node a, localBind   :: Node a                                         }
+type Identificator = (String, Int)
+
+data TypeError = TypeError -- ...
+
+data MonoType =
+    Variable    { varIdentifier :: Identificator , vConstraints :: S.HashSet String                  }
+  | Constructor { constructor   :: Node , typeArgs     :: [MonoType]                          }
+  | Function    { fromClass     :: Bool          , takesType    :: MonoType   , retsType :: MonoType }
+  | BuiltinNat | BuiltinReal | BuiltinChar | BuiltinString
+  | Bottom -- just for convencience
+  deriving (Eq)
+
+data Node =
+    Atom           { name       :: String, atomType    :: AtomType , info :: MonoType , pos :: SourcePos        }
+  | Lambda         { args       :: [Node], lamExpr     :: Node                                        }
+  | Expression     { expression :: [Node]                                                              }
+  | BinaryExpr     { leftExpr   :: Node , oper        :: Node  , rightExpr   :: Node               }
+  | ConcType       { concName   :: Node , consArgs    :: [Node]                                       }
+  | TypeAssertion  { node       :: Node , constraints :: H.HashMap String [Node], choritype :: Node }
+  | LocalBind      { locName    :: Node , localBind   :: Node                                         }
     deriving (Eq)
 
-instance Show (Node a) where
+instance Show Node where
   show (Atom n _ _ _)       = n
   show (Lambda a b)         = show "\\" ++ show a ++ " -> " ++ show b
   show (Expression [])      = undefined
@@ -38,20 +51,19 @@ instance Show (Node a) where
   show (TypeAssertion n c t)  = show n ++ " :: " ++ show c ++ " => "++ show t
   show (LocalBind v b)        = "let " ++ show v ++ " = " ++ show b
 
-instance Show (TopLevel a) where
+instance Show TopLevel where
   show (Implementation f a b) = show f ++ ' ' : show a ++ " = " ++ show b
   show (Declaration f c t)    = show f ++ " :: " ++ show c ++ " => " ++ show t
-  show (Data n a c)           = show n ++ show a ++ " with constructors " ++ show c
+  show (Data n c)           = show n ++ " with constructors " ++ show c
   show (Instance i c m)       = show i ++ " for " ++ show c ++ " where " ++ show m
   show (Class c d)            = show c ++ " where " ++ show d
   show Infix                  = ""
 
 type Precedence = Int
 type ParserState = (SourcePos, H.HashMap String Precedence)
-type NoType = ()
 
-atomWithName :: AtomType -> SourcePos -> String -> Node NoType
-atomWithName t s n = Atom n t () s
+atomWithName :: AtomType -> SourcePos -> String -> Node
+atomWithName t s n = Atom n t Bottom s
 
 scanChar :: (Monad m) => ParsecT String ParserState m Char -> ParsecT String ParserState m Char
 scanChar p = do
@@ -63,41 +75,41 @@ parseEscapingChars :: (Monad m) => ParsecT String ParserState m Char
 parseEscapingChars = scanChar (char '\\' >> escaping) <|> scanChar anyChar
   where escaping = oneOf "'\"\\nrtbfv" :: (Monad m) => ParsecT String ParserState m Char
 
-parseChar :: (Monad m) => ParsecT String ParserState m (Node NoType)
+parseChar :: (Monad m) => ParsecT String ParserState m Node
 parseChar = do
   pos' <- fst <$> getState
   atomWithName Char' pos' . (: []) <$> between (char '\'') (char '\'') parseEscapingChars
 
-parseString :: (Monad m) => ParsecT String ParserState m (Node NoType)
+parseString :: (Monad m) => ParsecT String ParserState m Node
 parseString = do
   pos' <- fst <$> getState
   atomWithName String' pos' <$> between (char '\"') (char '\"') (many parseEscapingChars)
 
-parseNum :: (Monad m) => ParsecT String ParserState m (Node NoType)
+parseNum :: (Monad m) => ParsecT String ParserState m Node
 parseNum = do
   pos' <- fst <$> getState
   int <- many1 $ scanChar digit
   dec <- option "" $ (:) <$> char '.' <*>  many1 digit
   return $ atomWithName (if null dec then Nat else Real') pos' $ int ++ dec
 
-parseIdentifier :: (Monad m) => ParsecT String ParserState m (Node NoType)
+parseIdentifier :: (Monad m) => ParsecT String ParserState m Node
 parseIdentifier = do
   pos' <- fst <$> getState
   initial <- scanChar lower
   atomWithName Identifier pos' . (initial :) <$> many (scanChar alphaNum)
 
-parseTypeK :: (Monad m) => ParsecT String ParserState m (Node NoType)
+parseTypeK :: (Monad m) => ParsecT String ParserState m Node
 parseTypeK = do
   pos' <- fst <$> getState
   initial <- scanChar upper
   atomWithName TypeK pos' . (initial :) <$> many (scanChar alphaNum)
 
-parseOp :: (Monad m) => ParsecT String ParserState m (Node NoType)
+parseOp :: (Monad m) => ParsecT String ParserState m Node
 parseOp = do
   pos' <- fst <$> getState
   atomWithName Operator pos' <$> many1 (scanChar $ oneOf "+-*/")
 
-parseLambda :: (Monad m) => ParsecT String ParserState m (Node NoType)
+parseLambda :: (Monad m) => ParsecT String ParserState m Node
 parseLambda = do
   _ <- char '\\' <|> char 'λ'
   spaces
@@ -106,10 +118,10 @@ parseLambda = do
   _ <- string "->"
   Lambda lambArgs <$> parseValueExpression
 
-parseAtom :: (Monad m) => ParsecT String ParserState m (Node NoType)
+parseAtom :: (Monad m) => ParsecT String ParserState m Node
 parseAtom = parseIdentifier <|> parseTypeK <|> between (char '(') (char ')') parseBinaryExpression <|> parseNum <|> parseChar <|> parseString
 
-parseExpression :: (Monad m) => ParsecT String ParserState m (Node NoType)
+parseExpression :: (Monad m) => ParsecT String ParserState m Node
 parseExpression = Expression <$> many1 (do {
   atom <- parseAtom;
   spaces;
@@ -117,7 +129,7 @@ parseExpression = Expression <$> many1 (do {
 })
 
 {- TODO: Infixl vs Infixr -}
-parseBinaryExpression :: (Monad m) => ParsecT String ParserState m (Node NoType)
+parseBinaryExpression :: (Monad m) => ParsecT String ParserState m Node
 parseBinaryExpression = do
   expr <- parseExpression
   spaces
@@ -130,10 +142,10 @@ parseBinaryExpression = do
   }  <|> return return
   mkRest expr
   where
-    addNode :: (Monad m) => H.HashMap String Int -> Node NoType -> Node NoType -> Node NoType -> m (Node NoType)
+    addNode :: (Monad m) => H.HashMap String Int -> Node -> Node -> Node -> m Node
     addNode prec op  expr' expr= return $ insertNode (H.findWithDefault 5 (name op) prec) prec op expr' expr
 
-    insertNode :: Int -> H.HashMap String Int -> Node NoType -> Node NoType -> Node NoType -> Node NoType
+    insertNode :: Int -> H.HashMap String Int -> Node -> Node -> Node -> Node
     insertNode _ _ op expr'@(Expression _) expr                         = BinaryExpr expr op expr'
     insertNode opPrec prec op expr'@(BinaryExpr lexpr' op' rexpr') expr = let opPrec' = H.findWithDefault 5 (name op') prec
                                                                             in if opPrec' > opPrec
@@ -141,7 +153,7 @@ parseBinaryExpression = do
                                                                                 else BinaryExpr (insertNode opPrec prec op lexpr' expr) op' rexpr'
     insertNode _ _ _ _ _ = undefined
 
-parseLocalBind :: (Monad m) => ParsecT String ParserState m (Node NoType)
+parseLocalBind :: (Monad m) => ParsecT String ParserState m Node
 parseLocalBind = do
   _ <- string "let"
   spaces
@@ -151,10 +163,10 @@ parseLocalBind = do
   spaces
   LocalBind bindedName <$> parseBinaryExpression
 
-parseValueExpression :: (Monad m) => ParsecT String ParserState m (Node NoType)
+parseValueExpression :: (Monad m) => ParsecT String ParserState m Node
 parseValueExpression = parseLocalBind <|> parseBinaryExpression <|> parseLambda
 
-parseImplementation :: (Monad m) => ParsecT String ParserState m (TopLevel NoType)
+parseImplementation :: (Monad m) => ParsecT String ParserState m TopLevel
 parseImplementation = do
   fName <- parseIdentifier <|> parseOp
   spaces
@@ -165,16 +177,16 @@ parseImplementation = do
   _ <- char ';'
   return $ Implementation fName fArgs bodyL
 
-parseConc :: (Monad m) => ParsecT String ParserState m (Node NoType)
+parseConc :: (Monad m) => ParsecT String ParserState m Node
 parseConc = do
   typeConstructor <- parseTypeK
   constructorArgs <- many $ parseIdentifier <|> between (char '(') (char ')') parseType
   return $ ConcType typeConstructor constructorArgs
 
-parseType :: (Monad m) => ParsecT String ParserState m (Node NoType)
+parseType :: (Monad m) => ParsecT String ParserState m Node
 parseType = parseIdentifier <|> parseConc
 
-parseConstraints :: (Monad m) => ParsecT String ParserState m (H.HashMap String [Node NoType])
+parseConstraints :: (Monad m) => ParsecT String ParserState m (H.HashMap String [Node])
 parseConstraints = between (char '(') (char ')' >> spaces >> string "=>") $ parseConstraint `chainl1` (char ',' >> return (H.unionWith (++)))
   where
   parseConstraint = do {
@@ -182,7 +194,7 @@ parseConstraints = between (char '(') (char ')' >> spaces >> string "=>") $ pars
     H.singleton (name cnstraint) . (: []) <$> parseIdentifier;
   }
 
-parseTypeParser :: (Monad m) => ParsecT String ParserState m (Node NoType) -> ParsecT String ParserState m (Node NoType)
+parseTypeParser :: (Monad m) => ParsecT String ParserState m Node -> ParsecT String ParserState m Node
 parseTypeParser p = do
   parsed <- p
   spaces
@@ -191,21 +203,20 @@ parseTypeParser p = do
   cnstraints <- option H.empty parseConstraints
   TypeAssertion parsed cnstraints . Expression <$> ((spaces *> parseType <* spaces) `sepBy` string "->") <* char ';'
 
-parseDeclaration :: (Monad m) => ParsecT String ParserState m (TopLevel NoType)
+parseDeclaration :: (Monad m) => ParsecT String ParserState m TopLevel
 parseDeclaration = (\(TypeAssertion e c t) -> Declaration e c t) <$> parseTypeParser (parseIdentifier <|> parseOp)
 
-parseData :: (Monad m) => ParsecT String ParserState m (TopLevel NoType)
+parseData :: (Monad m) => ParsecT String ParserState m TopLevel
 parseData = do
   _ <- string "data"
   spaces
   dataName <- parseIdentifier
   spaces
-  dataArgs <- many (parseIdentifier <* spaces)
   _ <- char '='
   spaces
-  Data dataName dataArgs <$> parseConc `sepBy` char '|'
+  Data dataName <$> parseConc `sepBy` char '|'
 
-parseInstance :: (Monad m) => ParsecT String ParserState m (TopLevel NoType)
+parseInstance :: (Monad m) => ParsecT String ParserState m TopLevel
 parseInstance = do
   _ <- string "instance"
   spaces
@@ -217,17 +228,17 @@ parseInstance = do
     ((parseImplementation <&> uncurry H.singleton . ((\(Instance n _ _) -> name n) &&& (: [])))
     `chainl1` (char ';' >> return (H.unionWith (++))))
 
-parseClass :: (Monad m) => ParsecT String ParserState m (TopLevel NoType)
+parseClass :: (Monad m) => ParsecT String ParserState m TopLevel
 parseClass = do
   _ <- string "class"
   spaces
   classNaming <- parseIdentifier
   spaces
   Class classNaming <$> between (char '{') (char '}')
-    ((parseDeclaration <&> uncurry H.singleton . ((\(Class n _) -> name n) &&& (: [])))
-    `chainl1` (char ';' >> return (H.unionWith (++))))
+    ((parseDeclaration <&> uncurry H.singleton . ((\(Class n _) -> name n) &&& id))
+    `chainl1` (char ';' >> return H.union))
 
-parseInfix :: (Monad m) => ParsecT String ParserState m (TopLevel NoType)
+parseInfix :: (Monad m) => ParsecT String ParserState m TopLevel
 parseInfix = do
   _ <- string "infix"
   spaces
@@ -239,11 +250,11 @@ parseInfix = do
   updateState $ second $ H.insert (name op) ((read :: String -> Int) prec)
   return Infix
 
-parseFunc :: (Monad m) => ParsecT String ParserState m (TopLevel NoType)
+parseFunc :: (Monad m) => ParsecT String ParserState m TopLevel
 parseFunc = parseImplementation <|> parseDeclaration
 
-parseChoriLang :: (Monad m) => ParsecT String ParserState m [TopLevel NoType]
+parseChoriLang :: (Monad m) => ParsecT String ParserState m [TopLevel]
 parseChoriLang = (parseFunc <|> parseData <|> parseDeclaration <|> parseInstance <|> parseClass <|> parseInfix) `sepBy` spaces
 
-parser :: (Monad m) => String -> m (Either ParseError [TopLevel NoType])
+parser :: (Monad m) => String -> m (Either ParseError [TopLevel])
 parser = runParserT parseChoriLang (initialPos "CommandLine", H.empty) "CommandLine"
--- src/Typechecker.hs
+++ src/Typechecker.hs
@@ -3,7 +3,7 @@
 {-# LANGUAGE TupleSections #-}
 module Typechecker where
 
-import Parser (TopLevel(..), Node(..), AtomType(..), NoType)
+import Parser (TopLevel(..), Node(..), AtomType(..), MonoType(..), Identificator, TypeError(..))
 import Data.Bool
 import Data.Function
 import Data.Functor
@@ -13,6 +13,16 @@ import Control.Monad.State
 import qualified Data.HashMap.Strict as H
 import qualified Data.HashSet as S
 
+-- Helpers
+
+fst3 :: (a, b, c) -> a
+snd3 :: (a, b, c) -> b
+thrd :: (a, b, c) -> c
+
+fst3 (a, _, _) = a
+snd3 (_, b, _) = b
+thrd (_, _, c) = c
+
 maybeToEither :: a -> Maybe b -> Either a b
 maybeToEither _ (Just b) = Right b
 maybeToEither a Nothing  = Left a
@@ -20,17 +30,7 @@ maybeToEither a Nothing  = Left a
 update :: MonadState a m => (a -> a) -> m a
 update f = modify f >> get
 
-type Identificator = (String, Int)
-
-data TypeError = TypeError -- ...
-
-data MonoType =
-    Variable    { varIdentifier :: Identificator , vConstraints :: S.HashSet String                  }
-  | Constructor { constructor   :: Node NoType , typeArgs     :: [MonoType]                          }
-  | Function    { fromClass     :: Bool          , takesType    :: MonoType   , retsType :: MonoType }
-  | BuiltinNat | BuiltinReal | BuiltinChar | BuiltinString
-  | Bottom -- just for convencience
-  deriving (Eq)
+--
 
 -- (Poly)Type
 data Type = Scheme { bounds :: H.HashMap Identificator (S.HashSet String), monoBounded :: MonoType }
@@ -42,6 +42,7 @@ data TheContext =
     _functionContext :: H.HashMap String Type,
     _typeContext     :: H.HashMap String [Type],
     _instanceContext :: H.HashMap String (S.HashSet String),
+    _classContext    :: H.HashMap String (S.HashSet String),
     _typeGenerator   :: MonoType
   }
 makeLenses ''TheContext
@@ -78,12 +79,12 @@ compose (Subs m1) (Subs m2) = Subs $ H.foldMapWithKey (\t t' m -> H.alter (\case
       Just t'' -> Just t''
     }) t m) m1 m2
 
-typeExprToType :: H.HashMap String [Node NoType] -> Node NoType -> StateT TheContext (Either TypeError) Type
+typeExprToType :: H.HashMap String [Node] -> Node -> StateT TheContext (Either TypeError) Type
 typeExprToType cnstraints typeExpr = do
   (tVars, monoExpr) <- toMono typeExpr
   return $ Scheme (H.mapKeys (, 0) (H.map (S.fromList . map name) cnstraints) `H.union` tVars) monoExpr
   where
-    toMono :: Node NoType -> StateT TheContext (Either TypeError) (H.HashMap Identificator (S.HashSet String), MonoType)
+    toMono :: Node -> StateT TheContext (Either TypeError) (H.HashMap Identificator (S.HashSet String), MonoType)
     toMono (Atom n _ _ _) = return (H.singleton (n, 0) S.empty, Variable (n, 0) S.empty)
     toMono (Expression tExpr) = foldl1 (\(v, m) (v', m') -> (v `H.union` v', Function False m m')) <$> mapM toMono tExpr
     toMono (ConcType i a) = do
@@ -93,6 +94,9 @@ typeExprToType cnstraints typeExpr = do
         Nothing    -> Left TypeError
       mapM toMono a <&> second (Constructor i) . sequence
     toMono _ = undefined
+
+constructorToType :: Node -> Type
+constructorToType = undefined
 
 freeInType :: Type -> H.HashMap Identificator (S.HashSet String)
 freeInType (Scheme bounds' monoBounded') = freeInType' monoBounded' `H.difference` bounds'
@@ -104,7 +108,7 @@ freeInType (Scheme bounds' monoBounded') = freeInType' monoBounded' `H.differenc
     freeInType' _                  = H.empty
 
 freeInCtx :: TheContext -> H.HashMap Identificator (S.HashSet String)
-freeInCtx (TheContext ctx _ _ _) = foldl (\s p -> s `H.union` freeInType p) H.empty ctx
+freeInCtx (TheContext ctx _ _ _ _) = foldl (\s p -> s `H.union` freeInType p) H.empty ctx
 
 unify :: MonoType -> MonoType -> StateT TheContext (Either TypeError) Subs
 unify v@(Variable s c)    v'@(Variable s' c')
@@ -139,68 +143,72 @@ instantiate (Scheme bounds' monoBounded')= do
       nBeta <- varIdentifier . _typeGenerator <$> update (typeGenerator `over` generateFrom)
       return $ (. map') $ typeMap $ singleMap (Variable k c) $ Variable nBeta c
 
-algoW :: Node NoType -> StateT TheContext (Either TypeError) (Subs, MonoType)
-algoW (Atom name' type' _ _) =
-  (mempty, ) <$> case type' of
-    Identifier -> getFunc
-    Operator   -> getFunc
-    Nat        -> return BuiltinNat
-    Real'      -> return BuiltinReal
-    Char'      -> return BuiltinChar
-    String'    -> return BuiltinString
+algoW :: Node -> StateT TheContext (Either TypeError) (Subs, MonoType, Node)
+algoW (Atom name' type' _ pos') =
+  assoc . (mempty, ) <$> case type' of
+    Identifier -> typeAndTyped <$> getFunc
+    Operator   -> typeAndTyped <$> getFunc
+    Nat        -> return $ typeAndTyped BuiltinNat
+    Real'      -> return $ typeAndTyped BuiltinReal
+    Char'      -> return $ typeAndTyped BuiltinChar
+    String'    -> return $ typeAndTyped BuiltinString
     _          -> undefined
   where getFunc = gets _functionContext
                   >>= lift . maybeToEither TypeError . H.lookup name'
                   >>= instantiate
+        assoc (a, (b, c)) = (a, b, c)
+        typeAndTyped m = (m, Atom name' type' m pos')
 
 algoW (Lambda args' expr') = do
   betas <- mapM addArg  args'
-  (sE, tE) <- algoW expr'
-  return (sE, typeMap sE $ foldl1 (Function False) (betas ++ [tE]))
+  (sE, tE, lT) <- algoW expr'
+  return (sE, typeMap sE $ foldl1 (Function False) (betas ++ [tE]), lT)
   where
-    addArg :: Node NoType -> StateT TheContext (Either TypeError) MonoType
+    addArg :: Node -> StateT TheContext (Either TypeError) MonoType
     addArg a = do
       beta <- _typeGenerator <$> update (typeGenerator `over` generateFrom)
       modify . (functionContext `over`) . H.insert (name a) . Scheme H.empty $ beta
       return beta
 
 algoW (Expression (e:es)) = do
-  (sF, tF) <- algoW e
+  (sF, tF, fT) <- algoW e
   modify $ functionContext `over` H.map (polyMap sF)
-  (sAs, tAs) <- foldM (\(sA, tAs) eArg -> do {
-      (sAn, tAn) <- algoW eArg;
+  (sAs, tAs, eT) <- foldM (\(sA, tAs, eTs) eArg -> do {
+      (sAn, tAn, eTn) <- algoW eArg;
       modify $ functionContext `over` H.map (polyMap sAn);
-      return (sA `compose` sAn, tAn : tAs)
-    }) (mempty, []) (reverse es)
+      return (sA `compose` sAn, tAn : tAs, eTn : eTs)
+    }) (mempty, [], []) (reverse es)
   beta <- _typeGenerator <$> update (typeGenerator `over` generateFrom)
   sExpr <- unify (typeMap sAs tF) (Function False (foldl1 (Function False) tAs) beta)
-  return (sExpr `compose` sAs `compose` sF, typeMap sExpr beta)
+  return (sExpr `compose` sAs `compose` sF, typeMap sExpr beta, Expression $ fT:eT)
 
 algoW (BinaryExpr lExpr op' rExpr) = algoW $ Expression [op', lExpr, rExpr]
 
 algoW (LocalBind locName' localBind') = do
-  (sB, tB) <- algoW localBind'
+  (sB, tB, bT) <- algoW localBind'
   ctx <- update (functionContext `over` H.map (polyMap sB))
   modify $ functionContext `over` H.insert (name locName') (Scheme (freeInCtx ctx) tB)
-  return (sB, tB)
+  return (sB, tB, bT)
 
 algoW (TypeAssertion e c t) = do
-  (sE, tE) <- algoW e
+  (sE, tE, bT) <- algoW e
   tE' <- typeExprToType c t >>= instantiate
   sE' <- unify tE tE'
-  return (sE' `compose` sE, typeMap sE' tE')
+  return (sE' `compose` sE, typeMap sE' tE', bT)
+
+algoW (ConcType {}) = undefined
 
 algoW _ = undefined
 
-typeOfExpr :: Node NoType -> StateT TheContext (Either TypeError) MonoType
-typeOfExpr = fmap snd . algoW
+typeOfExpr :: Node -> StateT TheContext (Either TypeError) MonoType
+typeOfExpr = fmap snd3 . algoW
 
-check :: TopLevel NoType -> StateT TheContext (Either TypeError) (TopLevel MonoType)
+check :: TopLevel -> StateT TheContext (Either TypeError) [TopLevel]
 
 check (Implementation fName fArgs bodyL) = do
   ctx <- get
   implType <- foldl1 (Function False) <$> mapM genBeta (fArgs ++ [fName])
-  lastSub <- foldM (\s b -> mappend s . fst <$> algoW b) mempty bodyL
+  (lastSub, typedBody) <- foldM (\(s, b') b -> ((s <>) *** (: b')) . (fst3 &&& thrd) <$> algoW b) (mempty, []) $ reverse bodyL
   free <- gets freeInCtx
   put ctx
   fSub <- case H.lookup (name fName) (_functionContext ctx) of
@@ -208,20 +216,49 @@ check (Implementation fName fArgs bodyL) = do
     --                          ^~~~ should be an equality function instead
     Nothing -> return lastSub
   modify $ functionContext `over` H.insert (name fName) (Scheme free $ typeMap fSub implType)
-  return undefined
+  return [Implementation fName fArgs typedBody]
   where
-    genBeta :: Node NoType -> StateT TheContext (Either TypeError) MonoType
+    genBeta :: Node -> StateT TheContext (Either TypeError) MonoType
     genBeta n = do
       beta <- _typeGenerator <$> update (typeGenerator `over` generateFrom)
       modify $ functionContext `over` H.insert (name n) (Scheme H.empty beta)
       return beta
 
-check (Declaration dName cnstr dType) = do
+check decl@(Declaration dName cnstr dType) = do
   declarationType <- typeExprToType cnstr dType
   modify $ functionContext `over` H.insert  (name dName) declarationType
-  return undefined
+  return [decl]
 
-check _ = undefined
+check dataD@(Data typeName' implementations') = do
+  modify $ instanceContext `over` H.insert (name typeName') S.empty
+  mapM_ (\conc@(ConcType n _) -> modify $ functionContext `over` H.insert (name n) (constructorToType conc)) implementations'
+  return [dataD]
 
-checker :: [TopLevel NoType] -> StateT TheContext (Either TypeError) [TopLevel MonoType]
-checker = traverse check . filter (\case { Infix -> False; _ -> True })
+check (Instance instancing' instanced' implementations') = do
+  ctx <- get
+  lift $ case H.lookup (name instancing') $ _classContext ctx of
+    Just class' -> bool (Left TypeError) (Right ()) $ class' == H.keysSet implementations'
+    Nothing     -> Left TypeError
+  impls <- mapM check (concatMap snd $ H.toList implementations') >>= mangle . concat
+  put ctx
+  modify $ instanceContext `over` H.alter (Just . \case { Just s -> S.insert (name instancing') s; Nothing -> S.singleton (name instancing');} ) (name instanced')
+  return impls
+  where mangle = undefined
+
+check (Class className' declarations') = do
+  classCtx <- gets _classContext
+  case H.lookup (name className') classCtx of
+    Just _ -> lift $ Left TypeError
+    Nothing -> modify $ classContext `over` H.insert (name className') (H.keysSet declarations')
+  mapM_ check declarations'
+  return []
+
+check Infix = undefined
+
+checker :: [TopLevel] -> StateT TheContext (Either TypeError) [TopLevel]
+checker = fmap concat . traverse check . filter (\case { Infix -> False; _ -> True })
+
+{-- TODO:
+ -  Rewrite: Node a -> Node
+ -  Adding functions to the toplevel context as name mangling
+ -}
